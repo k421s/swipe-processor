95215d943e01afea3b4ae2012dfb50db8583380c
karlton-suits <karltonsuits@gmail.com>
2022-09-11 16:48:22 -0400

    bug fix for MyWidget Traceback error

--- pyqt_hello_world.py
+++ pyqt_hello_world.py
@@ -1,36 +1,32 @@
 import sys
 import random
-
 from PySide6 import QtCore, QtWidgets, QtGui
 
-# class named MyWidget - extending QTWidget 
 class MyWidget(QtWidgets.QWidget):
-    def __init__(self): #constructor
-        super().__init__() # access to methods in super class of qtwidgets.qwidget
+    def __init__(self):
+        super().__init__()
 
         self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
-        
-        self.button = QtWidgets.QPushButton("Click Me!")
-        self.text - QtWidgets.Qlabel("Hello World", alignment=QtCore.Qt.AlignCenter)
-        
-        self.layout = QtWidgets.QvBoxLayout(self)
+
+        self.button = QtWidgets.QPushButton("Click me!")
+        self.text = QtWidgets.QLabel("Hello World",
+                                     alignment=QtCore.Qt.AlignCenter)
+
+        self.layout = QtWidgets.QVBoxLayout(self)
         self.layout.addWidget(self.text)
         self.layout.addWidget(self.button)
-        
+
         self.button.clicked.connect(self.magic)
+
+    @QtCore.Slot()
+    def magic(self):
+        self.text.setText(random.choice(self.hello))
         
-        @QtCore.Slot()
-        def magic(self):
-            self.text.setText(random.choice(self.hello))
-            
-            
 if __name__ == "__main__":
-    app = QtWidgets.QApplication([]) 
-    
+    app = QtWidgets.QApplication([])
+
     widget = MyWidget()
     widget.resize(800, 600)
     widget.show()
-    
+
     sys.exit(app.exec())
-    
-    
