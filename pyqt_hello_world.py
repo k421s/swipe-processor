5176baf543cfd41ca45fdee9d802c6d7cc7c6514
karlton-suits <karltonsuits@gmail.com>
2022-09-11 17:16:00 -0400

    comments

--- pyqt_hello_world.py
+++ pyqt_hello_world.py
@@ -1,30 +1,33 @@
 import sys
 import random
-from PySide6 import QtCore, QtWidgets, QtGui
+from PySide6 import QtCore, QtWidgets, QtGui #import qt python submodules 
 
-class MyWidget(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
+class MyWidget(QtWidgets.QWidget): #class extending qwidget
+    def __init__(self): # python 'constructor'
+        super().__init__() # COOPERATIVE MULTIPLE INHERITANCE -- MRO BUT WITH DIAMOND PROBLEMS WOO!
 
-        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"] #greeting pool
 
-        self.button = QtWidgets.QPushButton("Click me!")
-        self.text = QtWidgets.QLabel("Hello World",
-                                     alignment=QtCore.Qt.AlignCenter)
+        #add and align buttons
+        self.button = QtWidgets.QPushButton("Click Me!")
+        self.text = QtWidgets.QLabel("Hello World", alignment = QtCore.Qt.AlignCenter)
 
         self.layout = QtWidgets.QVBoxLayout(self)
         self.layout.addWidget(self.text)
         self.layout.addWidget(self.button)
 
+        #magic member function to choose random from greeting pool
         self.button.clicked.connect(self.magic)
 
+    #slot to recieve magic from button when clicked
     @QtCore.Slot()
     def magic(self):
         self.text.setText(random.choice(self.hello))
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
+    app = QtWidgets.QApplication([]) #initialize pyqt with no args
 
+    #define widget characterisics
     widget = MyWidget()
     widget.resize(800, 600)
     widget.show()
