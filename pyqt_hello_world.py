a9de1275af681079591e2e37b2d8d814a6ace450
karlton-suits <karltonsuits@gmail.com>
2022-09-16 15:37:47 -0400

    grid layout

--- pyqt_hello_world.py
+++ pyqt_hello_world.py
@@ -7,33 +7,30 @@ from PySide6 import QtCore, QtWidgets, QtGui #import qt python submodules
 class MyWidget(QtWidgets.QWidget): #class extending qwidget
     def __init__(self): # python 'constructor'
         super().__init__() # COOPERATIVE MULTIPLE INHERITANCE -- MRO BUT WITH DIAMOND PROBLEMS WOO!
-
-        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"] #greeting pool
-
-        #add and align buttons
-        self.button = QtWidgets.QPushButton("Click Me!")
-        self.text = QtWidgets.QLabel("Hello World", alignment = QtCore.Qt.AlignRight)
         
-        #attempting to add a 2nd button - it wont do anything yet
+        # define buttons
+        self.button1 = QtWidgets.QPushButton("Click Me!")
         self.button2 = QtWidgets.QPushButton("Second Click Me!")
         self.button3 = QtWidgets.QPushButton("third Click Me!")
         self.button4 = QtWidgets.QPushButton("fourth Click Me!")
         self.button5 = QtWidgets.QPushButton("fifth Click Me!")
-
-        # layout layer
-        self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
+        self.button6 = QtWidgets.QPushButton("sixth Click Me!")
         
-        #adding new button to layer:
-        self.layout.addWidget(self.button2)
-        self.layout.addWidget(self.button3)
-        self.layout.addWidget(self.button4)
-        self.layout.addWidget(self.button5)
 
+
+        # setting grid layout
+        self.layout = QtWidgets.QGridLayout(self)
+        
+        self.layout.addWidget((self.button1), 0, 0)
+        self.layout.addWidget((self.button2), 0, 1)
+        self.layout.addWidget((self.button3), 0, 2)
+        self.layout.addWidget((self.button4), 1, 0)
+        self.layout.addWidget((self.button5), 1, 1)
+        self.layout.addWidget((self.button6), 1, 2)
+        
 
         #magic member function to choose random from greeting pool
-        self.button.clicked.connect(self.magic)
+        self.button1.clicked.connect(self.magic)
 
     #slot to recieve magic from button when clicked
     @QtCore.Slot()
@@ -45,7 +42,7 @@ if __name__ == "__main__":
 
     #define widget characterisics
     widget = MyWidget()
-    widget.resize(800, 600)
+    #widget.resize(800, 600)
     widget.show()
 
     sys.exit(app.exec())
